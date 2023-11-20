a2642b39e3cfea29175d5df370936a702a9dd15b
irakibul7 <irakibul568@gmail.com>
2023-11-20 19:01:19 +0600

    add boilerplate of dxg website

---
+++ config/site.ts
@@ -0,0 +1,109 @@
+export const siteConfig = {
+  siteMetadata: {
+    title: 'Gatsby Starter Tailwind',
+    description:
+      'A Gatsby starter styled using Tailwind with dark mode enabled by default.',
+  },
+  mainNav: [
+    {
+      title: 'Home',
+      slug: '/',
+    },
+    {
+      title: 'Triple E Xperience',
+      slug: '/triple-e-xperience',
+    },
+    {
+      title: 'Who We Are',
+      slug: '/who-we-are',
+    },
+    {
+      title: 'Why DXG',
+      slug: '/why-dxg',
+    },
+    {
+      title: 'Services',
+      slug: '/services',
+    },
+    {
+      title: 'Contact Us',
+      slug: '/contact-us',
+    },
+  ],
+  footerNav: {
+    services: [
+      {
+        title: 'Production Management',
+        slug: '/',
+      },
+      {
+        title: 'Audio Visual Technology',
+        slug: '/',
+      },
+      {
+        title: 'Event Platform',
+        slug: '/',
+      },
+    ],
+    whyDXG: [
+      {
+        title: 'Our Process',
+        slug: '/',
+      },
+      {
+        title: 'Our Services',
+        slug: '/',
+      },
+      {
+        title: 'Our Mission',
+        slug: '/',
+      },
+      {
+        title: 'Our Experience',
+        slug: '/',
+      },
+    ],
+    legal: [
+      {
+        title: 'Privacy Policy',
+        slug: '/',
+      },
+      {
+        title: 'Terms of Use',
+        slug: '/',
+      },
+      {
+        title: 'Refund Policy',
+        slug: '/',
+      },
+    ],
+  },
+  socialLinks: [
+    {
+      title: 'Facebook',
+      url: '',
+    },
+    {
+      title: 'LinkedIn',
+      url: '',
+    },
+    {
+      title: 'Twitter',
+      url: '',
+    },
+    {
+      title: 'Instagram',
+      url: '',
+    },
+  ],
+  address: {
+    street: '',
+    city: '',
+    state: '',
+    zip: '',
+  },
+  contact: {
+    phone: '',
+    email: '',
+  },
+};
