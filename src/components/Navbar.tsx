7f2cd579e3c528c7e2e92dcf709ae1c7ea598408
abrarfahim19 <fahimpatwari19@gmail.com>
2023-11-22 10:50:54 +0600

    ci: brand color, headercolor, eslint and prettier configuartion added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,37 +1,23 @@
-import React from 'react';
-import MaxWidthWrapper from './MaxWidthWrapper';
-import Link from 'next/link';
-import { Button } from '@/components/ui/button';
+import { Button } from './ui/button';
 
-import { SITECONFIG } from '@/config/site';
-
+const links = [
+  'Home',
+  'Triple E Xperience',
+  'Who We Are',
+  'Services',
+  'Our Blog',
+  'Contact Us',
+];
 export default function Navbar() {
   return (
-    <div className="bg-slate-600 sticky z-50 top-0 inset-x-0 h-16">
-      <header className="relative bg-slate-600">
-        <MaxWidthWrapper>
-          <div className="">
-            <div className="flex justify-between h-16 items-center">
-              {/* TODO: Mobile nav */}
-              <div className="">
-                <Link href={'/'}>LOGO</Link>
-              </div>
-              <nav className="flex gap-6">
-                <div className="flex items-center">
-                  {SITECONFIG.mainNav.map((item, index) => (
-                    <Link key={index} href={item.slug} className="">
-                      {item.title}
-                    </Link>
-                  ))}
-                </div>
-                <Button variant="outline">
-                  Request for Proposal
-                </Button>
-              </nav>
-            </div>
-          </div>
-        </MaxWidthWrapper>
-      </header>
-    </div>
+    <header className='sticky inset-x-0 top-0 z-50 h-20 bg-header'>
+      <div>
+        {links.map((item, index) => (
+          <Button className='text-white' key={index} variant='link'>
+            {item}
+          </Button>
+        ))}
+      </div>
+    </header>
   );
 }
