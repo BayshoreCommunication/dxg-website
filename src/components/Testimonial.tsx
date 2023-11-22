f3b95ae5609b6f720b9f8dc914768bf0d34a595c
irakibul7 <irakibul568@gmail.com>
2023-11-22 15:05:25 +0600

    init testimonial part

---
+++ src/components/Testimonial.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MaxWidthWrapper from './MaxWidthWrapper';
+function Testimonial() {
+  return (
+    <div className='bg-header'>
+      <MaxWidthWrapper>
+        <div className=''></div>
+      </MaxWidthWrapper>
+    </div>
+  );
+}
+
+export default Testimonial;
