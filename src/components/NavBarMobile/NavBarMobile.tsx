a7428d9a941d07c4cfb9819eb5bd1382c997e445
abrarfahim19 <fahimpatwari19@gmail.com>
2023-11-22 18:39:30 +0600

    feat: header primarily done

---
+++ src/components/NavBarMobile/NavBarMobile.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import { SITECONFIG } from '@/config/site';
+import Link from 'next/link';
+import { MouseEvent, useState } from 'react';
+import { Button } from '../ui/button';
+
+export const NavBarMobile = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const dropDownShownHandler = () => {
+    setShowDropdown(true);
+  };
+  const dropDownDisableHandler = (e: MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    setShowDropdown(false);
+  };
+  return (
+    <div className='block md:hidden' onClick={dropDownShownHandler}>
+      <p className='text-white'>Burger</p>
+      <div
+        className={`fixed inset-0 h-screen w-screen bg-header ${
+          showDropdown ? 'block' : 'hidden'
+        }`}
+      >
+        <div
+          className={`absolute right-5 top-5 h-5 w-5 cursor-pointer bg-red-600`}
+          onClick={(e) => dropDownDisableHandler(e)}
+        ></div>
+        <div className='mt-10 flex flex-col items-center justify-center '>
+          {SITECONFIG.mainNav.map((item, index) => {
+            return (
+              <Link key={index} href={item.slug}>
+                <Button
+                  className='mt-5 text-2xl font-semibold text-white hover:text-brand active:text-brand'
+                  variant='link'
+                >
+                  {item.title}
+                </Button>
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
