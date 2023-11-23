641882b250af47ab28b4e57af2ab2cd313c4f5ef
irakibul7 <irakibul568@gmail.com>
2023-11-23 14:47:14 +0600

    fix file name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-// import Footer from '@/components/Footer';
 import TailwindIndicator from '@/components/TailwindIndicator';
 import { cn } from '@/lib/utils';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
