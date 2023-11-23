3e1e5ca53dc8e18e4498e03f4b3a4fd66bd8cf68
abrarfahim19 <fahimpatwari19@gmail.com>
2023-11-23 15:38:03 +0600

    Feat: Footer added Fix: Header icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
+import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
+import TailwindIndicator from '@/components/TailwindIndicator';
+import { cn } from '@/lib/utils';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import Footer from '@/components/Footer';
-import TailwindIndicator from '@/components/TailwindIndicator';
-import { cn } from '@/lib/utils';
-import Navbar from '@/components/Navbar';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -18,16 +18,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="h-full">
+    <html lang='en' className='h-full'>
       <body
-        className={cn(
-          'relative h-full font-sans antialiased',
-          inter.className
-        )}
+        className={cn('relative h-full font-sans antialiased', inter.className)}
       >
         <Navbar />
-        <main className="relative flex flex-col min-h-screen">
-          <div className="flex-grow flex-1">{children}</div>
+        <main className='relative flex min-h-screen flex-col'>
+          <div className='flex-1 flex-grow'>{children}</div>
         </main>
         <Footer />
 
