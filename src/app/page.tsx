c0a5297845b052592627f25e25f9d933562cd5c7
irakibul7 <irakibul568@gmail.com>
2023-11-26 19:04:15 +0600

    add home support section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,16 @@ import Testimonial from '@/components/Testimonial';
 import Overview from '@/components/home/Overview';
 import WhyDxg from '@/components/home/WhyDxg';
 import Services from '@/components/home/Services';
+import WeSupport from '@/components/home/WeSupport';
+
 export default function Home() {
   return (
     <div>
       <Overview />
-      <OurBlogSection />
+      <WeSupport />
       <Services />
       <WhyDxg />
+      <OurBlogSection />
       <Testimonial />
       <div className='bg-black'>
         <NewsLetter />
