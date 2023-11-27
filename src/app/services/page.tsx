1db8e2e54047ad939d05351b005eebe7e195d44c
Raihan Sarkar <83104019+alwaysraihan@users.noreply.github.com>
2023-11-27 15:53:58 +0600

    feat: services base

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -4,7 +4,7 @@ import Testimonial from '@/components/Testimonial';
 import NewsLetter from '@/components/NewsLetter';
 export default function Services() {
   return (
-    <div>
+    <div className='bg-black'>
       <BreadCrumb
         buttonTitle='Book an appoinment'
         description='Demo Digital Xperience Group (DXG), a pioneering division of WLJ Consulting, is a premier partner in transforming events into unforgettable experiences. With our roots firmly planted in the event hospitality industry. We pride ourselves on elevating the digital and production quality of in-person, virtual, and hybrid events.'
