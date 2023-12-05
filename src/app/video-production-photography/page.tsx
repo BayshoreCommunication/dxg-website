ac7c2f4b8b426635669abc720fb510dcedfd5d17
Gazi Monir-Uz-Zaman <ciphercall@gmail.com>
2023-12-05 13:01:36 +0600

    New Pages
    Added

---
+++ src/app/video-production-photography/page.tsx
@@ -0,0 +1,23 @@
+import NewsLetter from '@/components/NewsLetter';
+import Testimonial from '@/components/Testimonial';
+import WhyDxg from '@/components/home/WhyDxg';
+import {VideoProductionPhotographySection} from '@/components/Sections/VideoProductionPhotographySection';
+
+
+export default function GraphicsAnimation() {
+  return (
+    <div className='bg-black'>
+      <VideoProductionPhotographySection />
+      <div className="flex flex-wrap md:px-28">
+        <div className="w-1/2 p-4">
+          <p style={{color:'white', paddingTop:'4rem', paddingBottom:'4rem', }}>Demo Digital Xperience Group (DXG), a pioneering division of WLJ Consulting, is a premier partner in transforming events into unforgettable experiences. With our roots firmly planted in the event hospitality industry. We pride ourselves on elevating the digital and production quality of in-person, virtual, and hybrid events. Demo Digital Xperience Group (DXG), a pioneering division of WLJ Consulting, is a premier partner in transforming events into unforgettable experiences. With our roots firmly planted in the event hospitality industry. We pride ourselves onDemo Digital Xperience Group (DXG), a pioneering division of WLJ Consulting, is a premier partner in transforming events into unforgettable experiences. With our roots firmly planted in the event hospitality industry. We pride ourselves on elevating the digital and production quality of in-person, virtual, and hybrid events. Demo Digital Xperience Group (DXG), a pioneering division of WLJ Consulting, is a premier partner in transforming events into unforgettable experiences. With our roots firmly planted in the event hospitality industry. We pride ourselves on . .</p>
+        </div>
+        <div className="w-1/2 p-4">
+        <NewsLetter />
+        </div>
+      </div>      
+      <Testimonial />
+      <WhyDxg />
+    </div>
+  );
+}
---
+++ src/components/Sections/GraphicsAnimationSection/GraphicsAnimationSection.tsx
@@ -0,0 +1,17 @@
+import { BreadCrumbWithoutButton } from '@/components/BreadcrumbWithoutButton';
+
+export const GraphicsAnimationSection = () => {
+  return (
+    <BreadCrumbWithoutButton
+      titleProps={{
+        coloredText: 'Graphics',
+        postText: 'Animation',
+        texAlign: 'text-center',
+        variant: 'caps',
+      }}
+      buttonTitle='Book An Appointment'
+      description='Demo Digital Xperience Group (DXG), a pioneering division of WLJ Consulting, is a premier partner in transforming events into unforgettable experiences. With our roots firmly planted in the event hospitality industry. We pride ourselves on elevating the digital and production quality of in-person, virtual, and hybrid events.'
+      bgImage='banner/Graphics%20&%20Animation_bnr.png' // Add your image URL here
+    />
+  );
+};
