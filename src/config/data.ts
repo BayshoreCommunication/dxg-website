da19d7f639a3e984bde5d20634699268573d0e22
Gazi Monir-Uz-Zaman <ciphercall@gmail.com>
2023-12-31 12:54:32 +0600

    update
    blog data stylings changed

--- src/config/data.ts
+++ src/config/data.ts
@@ -1465,7 +1465,7 @@ export const RECENT_BLOG_POST = [
 
           <p style="font-size: 16px;">Don't be afraid to ask questions and push boundaries, share this article with your fellow planners. Let's empower each other to conquer the "convenient" and create events that are anything but ordinary.</p>              <br/>
 
-          <h2 style="font-size: 20px; font-weight: bold;">Meta- In-house AV companies: convenient or a creative killer? 10 shocking truths you need to know before you sign!</h2>
+          <h2 style="font-size: 18px; font-weight: bold;">Meta- In-house AV companies: convenient or a creative killer? 10 shocking truths you need to know before you sign!</h2>
 `,
           tag: ['Read More'],
         },
