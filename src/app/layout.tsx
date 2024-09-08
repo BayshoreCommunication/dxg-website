b09f7e8dcbfd57f03fc2b601f832b243aee6d55e
arsahak <arsahak99@gmail.com>
2024-09-08 17:22:18 +0600

    update og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,19 +8,33 @@ import './globals.css';
 import { SITECONFIG } from '@/config/site';
 const inter = Inter({ subsets: ['latin'] });
 
+// export const metadata: Metadata = {
+//   title: SITECONFIG.siteMetadata.title,
+//   description: SITECONFIG.siteMetadata.description,
+//   icons: {
+//     icon: '/favicon.ico',
+//     shortcut: '/favicon-16x16.png',
+//     apple: '/apple-icon.png',
+//   },
+//   openGraph: {
+//     images: '/opengraph-image.jpg',
+//   },
+// };
+
 export const metadata: Metadata = {
   title: SITECONFIG.siteMetadata.title,
   description: SITECONFIG.siteMetadata.description,
-  icons: {
-    icon: '/favicon.ico',
-    shortcut: '/favicon-16x16.png',
-    apple: '/apple-icon.png',
+  metadataBase: new URL('https://www.dxg.agency'),
+  alternates: {
+    canonical: '/',
+    languages: {
+      'en-US': '/en-USA',
+    },
   },
   openGraph: {
-    images: "/opengraph-image.jpg",
+    images: '/opengraph-image.jpg',
   },
 };
-
 
 export default function RootLayout({
   children,
@@ -30,7 +44,10 @@ export default function RootLayout({
   return (
     <html lang='en' className='h-full scroll-smooth bg-black'>
       <body
-        className={cn('relative h-full font-sans antialiased overflow-x-hidden', inter.className)}
+        className={cn(
+          'relative h-full overflow-x-hidden font-sans antialiased',
+          inter.className
+        )}
       >
         <Navbar />
         <main className='relative flex min-h-screen flex-col'>
