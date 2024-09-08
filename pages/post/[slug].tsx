3c181f539efa32da4edc01fa48cfba9bafeb424c
arsahak <arsahak99@gmail.com>
2024-09-08 17:03:09 +0600

    update

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -13,9 +13,32 @@ import Link from 'next/link';
 import '@/app/globals.css';
 import './myStyle.css';
 import GetAllBlogPost from '@/lib/GetAllBlogPost';
+import parse from 'html-react-parser';
 
+interface BlogPost {
+  slug: string;
+  title: string;
+  body: string;
+  excerpt: string;
+  featuredImage: {
+    image: {
+      url: string;
+    };
+  };
+}
 
-
+interface Metadata {
+  title: string;
+  description: string;
+  openGraph: {
+    title: string;
+    description: string;
+    images: string[];
+    url: string;
+    type: string;
+    site_name: string;
+  };
+}
 // Slugify function to convert title to URL-friendly slug
 function slugify(text: string) {
   return text
@@ -26,6 +49,55 @@ function slugify(text: string) {
     .replace(/\-\-+/g, '-') // Replace multiple - with single -
     .replace(/^-+/, '') // Trim - from start of text
     .replace(/-+$/, ''); // Trim - from end of text
+}
+
+export async function generateMetadata({
+  slug,
+}: {
+  slug: { slug: string };
+}): Promise<Metadata> {
+  const blogPostData = await GetAllBlogPost();
+
+  // Find blog post details by slug
+  const blogDetails: BlogPost | undefined = blogPostData?.data?.find(
+    (blogs: BlogPost) => blogs.slug === slug.slug
+  );
+
+  // If no blog is found, return default metadata
+  if (!blogDetails) {
+    return {
+      title: 'Blog not found',
+      description: 'No blog post available.',
+      openGraph: {
+        title: 'Blog not found',
+        description: 'No blog post available.',
+        images: [],
+        url: 'https://www.dxg.agency/post/not-found',
+        type: 'article',
+        site_name: 'Digital Xperience Group',
+      },
+    };
+  }
+
+  // Parse the blog body and extract description
+  let description: any[] = parse(blogDetails.body) as any[];
+
+  // Fallback to blogDetails.excerpt if description parsing fails
+  const parsedDescription =
+    description?.[0]?.props?.children?.toString() || blogDetails.excerpt;
+
+  return {
+    title: blogDetails.title,
+    description: parsedDescription,
+    openGraph: {
+      title: blogDetails.title,
+      description: parsedDescription,
+      images: [blogDetails.featuredImage?.image?.url || ''],
+      url: `https://www.dxg.agency/post/${blogDetails.slug}`,
+      type: 'article',
+      site_name: 'Digital Xperience Group',
+    },
+  };
 }
 
 // Replace this with your actual data fetching logic
--- src/components/BlogCard/BlogBigImageCard.tsx
+++ src/components/BlogCard/BlogBigImageCard.tsx
@@ -3,8 +3,7 @@ import Image from 'next/image';
 import { Button } from '../ui/button';
 import { formatTimestamp } from '@/lib/utils';
 import LazyDescription from './LazyDescription.client';
-import GetAllBlogPost from '@/lib/GetAllBlogPost';
-import parse from 'html-react-parser';
+
 
 interface BlogBigImageCardProps {
   _id: number;
@@ -12,80 +11,6 @@ interface BlogBigImageCardProps {
   title: string;
   featuredImage: string;
   body: string;
-}
-
-interface BlogPost {
-  slug: string;
-  title: string;
-  body: string;
-  excerpt: string;
-  featuredImage: {
-    image: {
-      url: string;
-    };
-  };
-}
-
-interface Metadata {
-  title: string;
-  description: string;
-  openGraph: {
-    title: string;
-    description: string;
-    images: string[];
-    url: string;
-    type: string;
-    site_name: string;
-  };
-}
-
-export async function generateMetadata({
-  params,
-}: {
-  params: { slug: string };
-}): Promise<Metadata> {
-  const blogPostData = await GetAllBlogPost();
-
-  // Find blog post details by slug
-  const blogDetails: BlogPost | undefined = blogPostData?.data?.find(
-    (blogs: BlogPost) => blogs.slug === params.slug
-  );
-
-  // If no blog is found, return default metadata
-  if (!blogDetails) {
-    return {
-      title: 'Blog not found',
-      description: 'No blog post available.',
-      openGraph: {
-        title: 'Blog not found',
-        description: 'No blog post available.',
-        images: [],
-        url: 'https://www.dxg.agency/post/not-found',
-        type: 'article',
-        site_name: 'Digital Xperience Group',
-      },
-    };
-  }
-
-  // Parse the blog body and extract description
-  let description: any[] = parse(blogDetails.body) as any[];
-
-  // Fallback to blogDetails.excerpt if description parsing fails
-  const parsedDescription =
-    description?.[0]?.props?.children?.toString() || blogDetails.excerpt;
-
-  return {
-    title: blogDetails.title,
-    description: parsedDescription,
-    openGraph: {
-      title: blogDetails.title,
-      description: parsedDescription,
-      images: [blogDetails.featuredImage?.image?.url || ''],
-      url: `https://www.dxg.agency/post/${blogDetails.slug}`,
-      type: 'article',
-      site_name: 'Digital Xperience Group',
-    },
-  };
 }
 
 export const BlogBigImageCard: React.FC<any> = ({
